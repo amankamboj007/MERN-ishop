be37cb86731bad09cfac6822819f1883426e9e1c
Aman kamboj <kamboj.aman32@gmail.com>
2022-10-11 02:16:00 +0530

    env update

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,13 +3,13 @@ const nodeMailer = require("nodemailer")
 
 const sendEmail = async (options) => {
     let transporter = nodeMailer.createTransport({
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 465,
+        service: SMTP_SERVICE,
+        host: SMTP_HOST,
+        port: SMTP_PORT,
         secure: true,
         auth: {
-            user: "ishop.mern@gmail.com",
-            pass: "nmxcblbrgmhlmkqq"
+            user: process.env.FROM_EMAIL,
+            pass: process.env.FROM_EMAIL_PASS
         }
 
     })
